089ef2b463bcbdf561f95c41dd389b1ce2ffcce1
Michał Szopiński <mszopinski@mssnt.pl>
2022-03-20 15:31:33 +0100

    Refactor entry code

--- firmware/main.c
+++ firmware/main.c
@@ -1,36 +1,32 @@
+#include "main.h"
+
 #include <avr/io.h>
 #include <avr/interrupt.h>
 
-#define DISABLE_XY PD7
-#define DISABLE_Z PB0
-#define START PB1
-
-#define Y_STEP PC4
-#define Y_DIR PC5
-#define X_STEP PC2
-#define X_DIR PC3
-#define Z_STEP PC0
-#define Z_DIR PC1
-
 int main() {
-	DDRD |= (1 << DISABLE_XY);
-	DDRB |= (1 << DISABLE_Z);
-	DDRC |= (1 << X_STEP);
-	DDRC |= (1 << X_DIR);
-	DDRC |= (1 << Y_STEP);
-	DDRC |= (1 << Y_DIR);
-	DDRC |= (1 << Z_STEP);
-	DDRC |= (1 << Z_DIR);
+	//configure ports
+	DISABLE_XY_DIR_PORT |= (1 << DISABLE_XY);
+	DISABLE_Z_DIR_PORT |= (1 << DISABLE_Z);
+	STEP_DIR_PORT |=
+		(1 << X_STEP)
+		| (1 << X_DIR)
+		| (1 << Y_STEP)
+		| (1 << Y_DIR)
+		| (1 << Z_STEP)
+		| (1 << Z_DIR);
 
-	TCCR0A |= (1 << WGM01); //CTC
-	TCCR0B |= (2 << CS00);	//div by 8
+	//configure timer
+	TCCR0A |= (1 << WGM01); //clear timer on compare
+	TCCR0B |= (2 << CS00);	//divide clock by 8
 	OCR0A = 63;
 	TIMSK0 |= (1 << OCIE0A); //enable timer
 
+	//enable global interrupts
 	sei();
 
-	PORTD |= (1 << DISABLE_XY);
-	PORTB |= (1 << DISABLE_Z);
+	//disable motors
+	DISABLE_XY_PORT |= (1 << DISABLE_XY);
+	DISABLE_Z_PORT |= (1 << DISABLE_Z);
 }
 
 ISR(TIMER0_COMPA_vect) {
---
+++ firmware/main.h
@@ -0,0 +1,20 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#define DISABLE_XY PD7
+#define DISABLE_XY_PORT PORTD
+#define DISABLE_XY_DIR_PORT DDRD
+#define DISABLE_Z PB0
+#define DISABLE_Z_PORT PORTB
+#define DISABLE_Z_DIR_PORT DDRB
+
+#define STEP_PORT PORTC
+#define STEP_DIR_PORT DDRC
+#define X_STEP PC2
+#define X_DIR PC3
+#define Y_STEP PC4
+#define Y_DIR PC5
+#define Z_STEP PC0
+#define Z_DIR PC1
+
+#endif
