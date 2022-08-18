4510f8497ea573332b2fc2bae725d17662c0cabf
Michał Szopiński <mszopinski@mssnt.pl>
2022-08-18 22:35:16 +0200

    Fix dropzone

--- software/src/renderer/components/dropzone.jsx
+++ software/src/renderer/components/dropzone.jsx
@@ -4,13 +4,21 @@ import "style/dropzone";
 import { VscFile } from "react-icons/vsc";
 
 export default function Dropzone({ onSubmit }) {
+    const handleDrop = event => {
+        if (event.dataTransfer.files[0])
+            onSubmit(event.dataTransfer.files[0]);
+    };
     const handleInputChange = event => {
         if (event.target.files[0])
             onSubmit(event.target.files[0]);
     };
 
     return (
-        <label className="dropzone">
+        <label
+            className="dropzone"
+            onDragOver={event => event.preventDefault()}
+            onDropCapture={handleDrop}
+        >
             <div className="drop-here">
                 <VscFile/>
                 <p>Drop file here</p>
